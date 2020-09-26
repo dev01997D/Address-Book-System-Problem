ff6fba9f550cb4a2ac04d179d0d314ebd9ea3b7f
dev01997D <devnandan01997@gmail.com>
2020-09-26 22:45:07 +0530

    Addded: Welcome message added in main class

---
+++ AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbooksystem;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Address Book Program in AddressBookMain class");
+	}
+}
