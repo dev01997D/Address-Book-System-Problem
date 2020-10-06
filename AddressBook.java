7f0743c240855328bd04e0d9f9e6e1c5b2fd02a5
dev01997D <devnandan01997@gmail.com>
2020-10-06 09:35:31 +0530

    Added: Ability to detect Duplicate contact being added to AddressBook library functionality added

---
+++ AddressBook.java
@@ -0,0 +1,63 @@
+package com.bridgelabz.addressbooksystem;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+
+	private ArrayList<Contact> addressBook = new ArrayList<Contact>();
+
+	public ArrayList<Contact> getAddressBook() {
+		return this.addressBook;
+	}
+
+	public void setAddressBook(ArrayList<Contact> addressBook) {
+		this.addressBook = addressBook;
+	}
+
+	public void addContactToAddressBook(Contact contact) {
+		addressBook.add(contact);
+	}
+
+	public Contact editContact(String firstName, String lastName) {
+		Contact contactSearch = null;
+		for (Contact contact : addressBook) {
+			if (contact.getFirstName().equals(firstName) && contact.getLastName().equals(lastName)) {
+				contactSearch = contact;
+				break;
+			}
+		}
+
+		return contactSearch;
+	}
+
+	public boolean removeContact(String firstName, String lastName) {
+		boolean remove = false;
+		for (Contact contact : addressBook) {
+			if (contact.getFirstName().equals(firstName) && contact.getLastName().equals(lastName)) {
+				addressBook.remove(contact);
+				remove = true;
+				break;
+			}
+		}
+		return remove;
+	}
+
+	public boolean checkDuplicateContact(String firstName, String lastName) {
+		boolean duplicate = false;
+		for (Contact c : addressBook) {
+			if ((c.getFirstName()).equals(firstName) && (c.getLastName()).equals(lastName)) {
+				duplicate = true;
+				break;
+			}
+		}
+		return duplicate;
+	}
+
+	public void printContactDetails() {
+		for (Contact c : addressBook) {
+			System.out.println("First name: " + c.getFirstName() + " Last name: " + c.getLastName() + " Address: "
+					+ c.getAddress() + " City Name: " + c.getCityName() + " Zip Number: " + c.getZipNumber()
+					+ " Phone Number: " + c.getPhoneNumber() + " Email Id: " + c.getEmailId());
+		}
+	}
+}
--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,84 +1,155 @@
 package com.bridgelabz.addressbooksystem;
+
 import java.util.Scanner;
 
 public class AddressBookMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Address Book Program in AddressBookMain class");
 
-		//contact AddressBookLog object
-		AddressBookLog addressLogObj=new AddressBookLog();
-
-		//Variables
-		int zipNumber, selectMenu;
-		String firstName, firstNameEdit, firstNameDelete, lastName,address,cityName,emailId;
-		long phoneNumber;
-		char choice=' ';
-		int ch1=0;
+		// Display Welcome Message
+		System.out.println("Welcome to Address Book Program");
 
 		Scanner sc = new Scanner(System.in);
 
-		while(ch1!=2){
-			AddressBookDict abd=new AddressBookDict();
-			System.out.println("Please Enter from the Menu.\n1. Add Address Book \n2. Exit");
-			ch1=sc.nextInt();
-			if(ch1==2)
-				System.exit(0);
-			System.out.println("Enter the Address Book Name: ");
-			String addressBookName=sc.next();
-			
-			System.out.println("Please Enter from the Menu.\n1. Add a Contact: \n2. Edit a contact using the First Name: \n3. Delete a Contact using first name. \n4. Print all the contact details \n5. Exit");
-			selectMenu=sc.nextInt();
-			sc.nextLine();
-			do {
-				switch (selectMenu) {
-				case 1:
-					System.out.println("Enter the First Name:");
-					firstName = sc.nextLine();
-					System.out.println("Enter the Last Name:");
-					lastName = sc.nextLine();
-					System.out.println("Enter the Address");
-					address=sc.nextLine();
-					System.out.println("Enter the City Name");
-					cityName=sc.nextLine();
-					System.out.println("Enter the Zip Number of Address");
-					zipNumber=sc.nextInt();
-					System.out.println("Enter the Phone No:");
-					phoneNumber = sc.nextLong();
-					System.out.println("Enter the Email:");
-					sc.nextLine();
-					emailId = sc.nextLine();
+		// Instantiation of AddressBookDictionary Class
+		AddressBookDictionary addressBookDictionary = new AddressBookDictionary();
 
-					//Object of Contact class
-					Contact contactObj=new Contact(firstName,lastName,address,cityName,zipNumber,phoneNumber,emailId);
-					//Invoking addContact method in AddressBookLog
-					addressLogObj.addContact(contactObj);
-					
-					abd.addAddressBook(addressBookName, contactObj);    //Added the contact object in the addressbook
-					break;
-				case 2:
-					System.out.println("Enter the First Name of the contact you want to edit:");
-					firstNameEdit=sc.nextLine();
-					addressLogObj.editContactDetails(firstNameEdit);
-					System.out.println("Contact details updated SuccessFully.");
-					break;
-				case 3:
-					System.out.println("Enter the First Name of the contact you want to Delete:");
-					firstNameDelete=sc.nextLine();
-					addressLogObj.deleteContactDetails(firstNameDelete);
-					System.out.println("Contact details deleted SuccessFully.");
-					break;	
-				case 4:
-					addressLogObj.printContactDetails();
-				default:
-					System.exit(0);
-				}
-				System.out.println("Do you want to enter another contact details, Enter 'Y' for yes and 'N' for not.");
-				choice=sc.next().charAt(0);	
+		while (true) {
+			System.out.println("Please Select From Menu: \n1. Add New Address Book \n2. Exit:");
+			int select = sc.nextInt();
+
+			if (select == 1) {
+				System.out.println("Enter the Address Book Name : ");
 				sc.nextLine();
-			}while(choice=='y' || choice=='Y');
-		}
+				String addressBookName = sc.nextLine();
 
+				// Instantiation of AddressBook Class
+				AddressBook addressBookObj = new AddressBook();
+
+				addressBookDictionary.addAddressBook(addressBookName, addressBookObj);
+
+				int option = 0;
+
+				do {
+
+					System.out.println(
+							"Please Select From Menu: \n1 : Add Contact to Address Book\n2 : Update Existing Contact\n3 : Remove Contact"
+									+ "\n4 : Print contact details\n5. Exit");
+					option = sc.nextInt();
+
+					switch (option) {
+					// To Take Console Input for Contact Details
+					case 1:
+						System.out.println("Enter First Name : ");
+						String firstName = sc.next();
+						System.out.println("Enter Last Name : ");
+						String lastName = sc.next();
+						sc.nextLine();
+						System.out.println("Enter Address : ");
+						String address = sc.nextLine();
+						System.out.println("Enter City : ");
+						String city = sc.nextLine();
+						System.out.println("Enter Zip Code: ");
+						int zipCode = sc.nextInt();
+						System.out.println("Enter Phone Number : ");
+						Long phoneNo = sc.nextLong();
+						System.out.println("Enter Email ID : ");
+						String email = sc.next();
+						boolean duplicate = addressBookObj.checkDuplicateContact(firstName, lastName);
+						if (duplicate == true) {
+							System.out.println("Duplicate Entry. Try Again");
+						} else {
+							// Instantiation of Contact Class
+							Contact contactObj = new Contact(firstName, lastName, address, city, zipCode, phoneNo,
+									email);
+							// Add New Contact into Address Book
+							addressBookObj.addContactToAddressBook(contactObj);
+						}
+						break;
+
+					case 2: // Console input to take First and Last Names to Update Contact
+						System.out.println("Enter your First Name to Edit your Contact : ");
+						String firstNameEdit = sc.next();
+						System.out.println("Enter your Second Name to Edit your Contact : ");
+						String lastNameEdit = sc.next();
+
+						// Updating Contact
+						Contact contactEdited = addressBookObj.editContact(firstNameEdit, lastNameEdit);
+						if (contactEdited == null) {
+							System.out.println("Contact Not Available. Please try Again");
+						} else {
+
+							System.out.println(
+									"Select the Option you want to Update\n1 : First Name\n2 : Last Name\n3 : Address\n"
+											+ "4 : City\n5 : Zip\n6 : Phone Number\n7 : Email\nSelect :");
+							int choice = sc.nextInt();
+
+							switch (choice) {
+							case 1:
+								System.out.println("Enter the new First Name to Update : ");
+								String newFirstName = sc.next();
+								contactEdited.setFirstName(newFirstName);
+								break;
+							case 2:
+								System.out.println("Enter the new Last Name to Update : ");
+								String newLastName = sc.next();
+								contactEdited.setLastName(newLastName);
+								break;
+							case 3:
+								System.out.println("Enter the new Address to Update : ");
+								sc.nextLine();
+								String newAddress = sc.nextLine();
+								contactEdited.setAddress(newAddress);
+								break;
+							case 4:
+								System.out.println("Enter the new City to Update : ");
+								sc.nextLine();
+								String newCity = sc.nextLine();
+								contactEdited.setCityName(newCity);
+								break;
+							case 6:
+								System.out.println("Enter the new Zip to Update : ");
+								int newZip = sc.nextInt();
+								contactEdited.setZipNumber(newZip);
+								break;
+							case 7:
+								System.out.println("Enter the new Phone Number to Update : ");
+								Long newPhoneNo = sc.nextLong();
+								contactEdited.setPhoneNumber(newPhoneNo);
+								break;
+							case 8:
+								System.out.println("Enter the new Email to Update : ");
+								String newEmail = sc.next();
+								contactEdited.setEmailId(newEmail);
+								break;
+							default:
+								System.out.println("Enter Valid Number");
+								break;
+							}
+						}
+						break;
+
+					case 3: // Console input to take First and Last Names to Remove Contact
+						System.out.println("Enter your First Name of Contact : ");
+						String removeFirstName = sc.next();
+						System.out.println("Enter your Last Name to Contact : ");
+						String removeLastName = sc.next();
+						boolean remove = addressBookObj.removeContact(removeFirstName, removeLastName);
+						if (remove)
+							System.out.println("Contact Sucessfully Removed");
+						else
+							System.out.println("Contact not Available. Please Try Again!");
+						break;
+
+					case 4:
+						addressBookObj.printContactDetails();
+						break;
+					default:
+						break;
+					}
+				} while (option != 4);
+			} else
+				System.exit(0);
+		}
 	}
 }
